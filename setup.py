22b2f7bd3e51c002d6da17561f2174f719e3d340
Valentin Goldité <valentin.goldite@gmail.com>
2022-05-20 19:17:23 +0200

    :arrow_backward: back to dynamic version

--- setup.py
+++ setup.py
@@ -18,6 +18,15 @@ Copyright (C) 2022  Reactive Reality
 
 from setuptools import setup
 
+try:
+    # within pypi deployment docker container
+    import deploytools
+    version = deploytools.get_version()
+except ModuleNotFoundError:
+    # fallback for local testing
+    with open('../.rrci/version.yml', 'r') as f:
+        version = f.read().split('version: ')[1].strip()
+
 setup(
     name='rr-ml-config',
     version='1.3.0',
